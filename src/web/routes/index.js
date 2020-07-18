e17c940addd96c0e616baa2e83142af3637fc2c6
WilkRhu <wilk.caetano@gmail.com>
2020-07-18 20:21:24 -0300

    return error roter

--- src/web/routes/index.js
+++ src/web/routes/index.js
@@ -6,5 +6,10 @@ const auth = require("../../middleware/auth");
 router.post("/singUp", userController.create);
 router.post("/singIn", userController.singIn);
 router.get("/user/:id", auth, userController.findId);
+router.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
 
 module.exports = router;
